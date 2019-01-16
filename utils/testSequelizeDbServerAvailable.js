8da0563013ba6654a61059f305dc0b38b87dd348
Dr. Asis Hallab <asis.hallab@gmail.com>
2019-01-16 14:56:35 +0100

    Using Sequelize's 'authenticate()' to test connection.

--- utils/testSequelizeDbServerAvailable.js
+++ utils/testSequelizeDbServerAvailable.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
 const path = require('path')
+const Sequelize = require(path.join(__dirname, '..', 'connection.js'))
 
-try {
-  let Sequelize = require(path.join(__dirname, '..', 'connection.js'))
-  return process.exit(0)
-} catch (exception) {
-  console.log('Could not connect to relational database server.')
-  return process.exit(1)
+async function checkConnection() {
+  try {
+    await Sequelize.authenticate()
+    return process.exit(0)
+  } catch (exception) {
+    return process.exit(1)
+  }
 }
+
+checkConnection()
