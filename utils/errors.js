036c97767dd536183f7412fff66e1a10aac9e534
vsuaste <suaste@cimat.mx>
2019-02-21 00:24:20 -0600

    delete log commands

--- utils/errors.js
+++ utils/errors.js
@@ -9,13 +9,8 @@ class customArrayError extends Error {
 
 handleError = function(error){
   if(error.name === "SequelizeValidationError"){
-      console.log("Validation error")
-      let errorC = new customArrayError(error.errors, "Validation error");
-      console.log( errorC );
-      throw errorC;
-
+      throw new customArrayError(error.errors, "Validation error");
   }else{
-      console.log('Other error')
       throw new Error(error)
   }
 }
