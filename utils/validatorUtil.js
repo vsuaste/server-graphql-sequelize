24e61e6d096c61851b400eb83218dbb843734e7c
JKolokoltsev <j.kolokoltsev@gmail.com>
2019-03-05 00:53:01 -0600

    Closed #53 sequelize independent validations

---
+++ utils/validatorUtil.js
@@ -0,0 +1,18 @@
+
+/**
+ * ifHasValidatorFunctionInvoke - checks if data model has a validator function with
+ * the given name, and apply that function
+ *
+ * @param  {string} validatorFunction Name of the validator function
+ * @param  {object} dataModel The empty data model object
+ * @param  {object} data JSON data to be inserted into the dataModel
+ * @return {Error}  An error object if data is invalid or null otherwise
+ *
+ */
+
+module.exports.ifHasValidatorFunctionInvoke = function( validatorFunction, dataModel, data) {
+    if (typeof dataModel.prototype[validatorFunction] === "function") {
+        return dataModel.prototype[validatorFunction](data).error;
+    }
+};
+
