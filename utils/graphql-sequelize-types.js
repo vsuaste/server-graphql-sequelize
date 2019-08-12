cdd0299992646e6a30141a0cdf00f1ccdb502654
vsuaste <suaste@cimat.mx>
2019-08-12 18:03:34 -0500

    missing comma in dictionary

--- utils/graphql-sequelize-types.js
+++ utils/graphql-sequelize-types.js
@@ -5,7 +5,7 @@ Data types dictionary from graphql-type to sequelize-type
 module.exports = {
 
   "Int" : 'INTEGER',
-  "String": 'TEXT'
+  "String": 'TEXT',
   "Float": 'FLOAT',
   "Boolean": 'BOOLEAN',
   "Date": "DATEONLY",
