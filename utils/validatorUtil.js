11a0fc6a68ac09985961693bd56f80fc34d04cf2
Dr. Asis Hallab <asis.hallab@gmail.com>
2019-08-20 18:03:35 -0500

    Added util function to define AJV ISO Date-Time keywords.

--- utils/validatorUtil.js
+++ utils/validatorUtil.js
@@ -1,3 +1,4 @@
+const gd = require('graphql-iso-date')
 
 /**
  * ifHasValidatorFunctionInvoke - checks if data model has a validator function with
@@ -10,7 +11,6 @@
  *                   undefined if no validator was found to be registered
  *
  */
-
 module.exports.ifHasValidatorFunctionInvoke = async function( validatorFunction, dataModel, data) {
     if (typeof dataModel.prototype[validatorFunction] === "function") {
       try{
@@ -20,3 +20,87 @@ module.exports.ifHasValidatorFunctionInvoke = async function( validatorFunction,
       }
     }
 };
+
+/**
+ * Adds AJV asynchronous keywords to the argument AJV instance that define ISO
+ * Date, ISO Time, and ISO DateTime strings or the respective GraphQL instances
+ * (see package 'graphql-iso-date'). Use in a schema as in the following
+ * example: let schema = { '$async': true, properties: { startDate: { isoDate:
+ * true } } }
+ *
+ * @param {object} ajv - An instance of AJV (see package 'ajv' for details.
+ *
+ * @return {object} the modified instance of ajv. As Javascript uses references
+ * this return value can be ignored, as long as the original argument is kept
+ * and used.
+ */
+module.exports.addDateTimeAjvKeywords = function(ajv) {
+  ajv.addKeyword('isoDate', {
+    async: true,
+    compile: function(schema, parentSchema) {
+      return async function(data) {
+        try {
+          gd.GraphQLDate.serialize(data);
+          return true
+        } catch (e) {
+          return new Promise(function(resolve, reject) {
+            return reject(new Ajv.ValidationError([{
+              keyword: 'isoDate',
+              message: 'Must be a GraphQLDate instance or a ISO Date formatted string (e.g. "1900-12-31").',
+              params: {
+                'keyword': 'isoDate'
+              }
+            }]))
+          })
+        }
+      }
+    },
+    errors: true
+  })
+
+  ajv.addKeyword('isoTime', {
+    async: true,
+    compile: function(schema, parentSchema) {
+      return async function(data) {
+        try {
+          gd.GraphQLTime.serialize(data);
+          return true
+        } catch (e) {
+          return new Promise(function(resolve, reject) {
+            return reject(new Ajv.ValidationError([{
+              keyword: 'isoTime',
+              message: 'Must be a GraphQLTime instance or a ISO Time formatted string (e.g. "13:56:45Z" or "13.56.45.1982Z").',
+              params: {
+                'keyword': 'isoTime'
+              }
+            }]))
+          })
+        }
+      }
+    },
+    errors: true
+  })
+
+  ajv.addKeyword('isoDateTime', {
+    async: true,
+    compile: function(schema, parentSchema) {
+      return async function(data) {
+        try {
+          gd.GraphQLTime.serialize(data);
+          return true
+        } catch (e) {
+          return new Promise(function(resolve, reject) {
+            return reject(new Ajv.ValidationError([{
+              keyword: 'isoDateTime',
+              message: 'Must be a GraphQLDateTime instance or a ISO Date-Time formatted string (e.g. "1900-12-31T23:59:59Z" or "1900-12-31T23:59:59.1982Z").',
+              params: {
+                'keyword': 'isoDateTime'
+              }
+            }]))
+          })
+        }
+      }
+    },
+    errors: true
+  })
+}
