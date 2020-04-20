86e85eced8f5d5474b5ea65507d8f326f8f9dac2
Thomas Voecking <thomas.voecking@auticon.de>
2020-04-20 15:34:57 +0200

    Add function 'sanitizeAssociationArguments'

--- test/test.js
+++ test/test.js
@@ -105,3 +105,29 @@ describe('Unique', function() {
         expect(helper.unique([2, 3, 2, 4, 1, 5])).to.deep.equal([2, 3, 4, 1, 5]);
     })
 })
+
+describe('Sanitize association arguments', function() {
+    it('1, NOP for already sane arguments', function() {
+        const firstArguments = {addDogs: [4, 2], addCats: 1, addHamsters: 2};
+        let originalArguments = Object.assign({}, firstArguments);
+        let newArguments = helper.sanitizeAssociationArguments(originalArguments, ['addDogs', 'addCats', 'addHamsters']);
+        expect(newArguments).to.deep.equal(firstArguments);
+        expect(originalArguments).to.deep.equal(firstArguments);
+    })
+
+    it('2. One argument to be sanitized', function() {
+        const firstArguments = {addDogs: [4, 2, 4, 3], addCats: 1, addHamsters: 2};
+        let originalArguments = Object.assign({}, firstArguments);
+        let newArguments = helper.sanitizeAssociationArguments(originalArguments, ['addDogs', 'addCats', 'addHamsters']);
+        expect(newArguments).to.deep.equal({addDogs: [4, 2, 3], addCats: 1, addHamsters: 2});
+        expect(originalArguments).to.deep.equal(firstArguments);
+    })
+
+    it('3. All arguments to be sanitized', function() {
+        const firstArguments = {addDogs: [4, 2, 4, 3], addCats: [1, 1, 2], addHamsters: [2, 2]};
+        let originalArguments = Object.assign({}, firstArguments);
+        let newArguments = helper.sanitizeAssociationArguments(originalArguments, ['addDogs', 'addCats', 'addHamsters']);
+        expect(newArguments).to.deep.equal({addDogs: [4, 2, 3], addCats: [1, 2], addHamsters: [2]});
+        expect(originalArguments).to.deep.equal(firstArguments);
+    })
+})
