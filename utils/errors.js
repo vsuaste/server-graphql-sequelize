9efc69cbfc1283f3ab7f51108cd5509d0ed316d4
Thomas Voecking <thomas.voecking@auticon.de>
2020-05-15 17:13:24 +0200

    Replace the customArrayError with a customError that directly references to the OriginalError of a GraphQLError

--- utils/errors.js
+++ utils/errors.js
@@ -1,25 +1,20 @@
 const { GraphQLError } = require('graphql');
-const util = require('util');
 
-class customArrayError extends GraphQLError {
-  constructor(errors_array, message){
-    super(message, null, null, null, null, {errors: errors_array});
+class customError extends GraphQLError {
+  constructor(originalError, message){
+    super(message, null, null, null, null, originalError);
   }
 }
 
 handleError = function(error){
-  if(error.message === 'validation failed'){
-    throw new customArrayError(error.errors, error.message);
+  if(error.message){
+    throw new customError(error, error.message);
   }else if(error.name === "SequelizeValidationError"){
-      throw new customArrayError(error.errors, "Validation error");
+      throw new customError(error, "Validation error");
   }else if(error.code === 'ECONNABORTED' && error.url!== undefined){
     throw new GraphQLError(`Time out exceeded trying to reach server ${error.url}`);
   }else{
-    if (error.message) {
-      throw new GraphQLError(error.message, null, null, null, null, error);
-    }else {
-      throw new Error(error)
-    }
+    throw new Error(error)
   }
 }
 module.exports = { handleError}
