cebd42618f89001475cc48b9161fe8141706c291
Thomas Voecking <thomas.voecking@auticon.de>
2020-05-19 10:49:16 +0200

    Add new function 'stringifyCompletely'

--- utils/errors.js
+++ utils/errors.js
@@ -10,7 +10,21 @@ errorString = function(errorArray) {
   return errorArray.reduce((acc, curr) => acc + ', ' + curr.message, '');
 }
 
+stringifyCompletely = function(error, replacer, space) {
+  let allKeys = Reflect.ownKeys(error);
+  let errorMap = {};
+  for (let key of allKeys) {
+    errorMap[`${key}`] = error[`${key}`];
+  }
+  return JSON.stringify(errorMap, replacer, space);
+}
+
 handleError = function(error){
+  console.log('§§§§§§§§§§§§§§§§§§§§§§§§§§§');
+  console.log('§   Complete error output §');
+  console.log(stringifyCompletely(error, null, 4));
+  console.log('§ / Complete error output §');
+  console.log('§§§§§§§§§§§§§§§§§§§§§§§§§§§');
   if(error.message){
     let additionals = '';
     if (error.errors) {
