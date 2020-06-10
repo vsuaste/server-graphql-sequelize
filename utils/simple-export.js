2242192ff717a14ef47557347cdb1c7093e828c0
vsuaste <suaste@cimat.mx>
2020-06-09 20:40:19 -0500

    simple export fix for ddm

--- utils/simple-export.js
+++ utils/simple-export.js
@@ -17,12 +17,6 @@ crateHeaderCSV = function(attributes){
   return str_header;
 }
 
-asyncForEach = async function(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
-
 jsonToCSV = function(row_data, attributes){
   let str_csv = "";
   attributes.forEach( att => {
@@ -44,52 +38,60 @@ function wait(ms) {
 
 
 module.exports = async function(context, body_info, writableStream ){
-      //get resolver name for model
-      let model_name = body_info.model;
-      let getter_resolver = inflection.pluralize(model_name.slice(0,1).toLowerCase() + model_name.slice(1, model_name.length));
+      try{
+        //get resolver name for model
+        let model_name = body_info.model;
+        let getter_resolver = inflection.pluralize(model_name.slice(0,1).toLowerCase() + model_name.slice(1, model_name.length)) + 'Connection';
 
-      //get count resolver
-      let count_resolver = 'count'+inflection.pluralize(model_name.slice(0,1).toUpperCase() + model_name.slice(1, model_name.length));
-      let total_records = await resolvers[count_resolver]({}, context);
-      console.log("TOTAL NUMBER OF RECORDS TO STREAM: ", total_records);
+        //get count resolver
+        let count_resolver = 'count'+inflection.pluralize(model_name.slice(0,1).toUpperCase() + model_name.slice(1, model_name.length));
+        let total_records = await resolvers[count_resolver]({}, context);
+        console.log("TOTAL NUMBER OF RECORDS TO STREAM: ", total_records);
 
-      //pagination
-      let batch_step = {
-        limit: 1,
-        offset: 0
-      }
-
-      // http send stream header
-      let timestamp = new Date().getTime();
-      writableStream.writeHead(200, {'Content-Type': 'application/force-download',
-          'Content-disposition': `attachment; filename = ${timestamp}.csv`});
-
-      //get attributes names
-      let attributes = getAttributes(model_name);
-
-      //write csv header
-      let csv_header = crateHeaderCSV(attributes);
-      await writableStream.write(csv_header);
-
-      while(batch_step.offset < total_records){
-
-        try{
-           data = await resolvers[getter_resolver]({pagination: batch_step},context);
-
-           await asyncForEach(data, async (record) =>{
-              let row = jsonToCSV(record.dataValues, attributes);
-              await  writableStream.write(row);
-           })
-          batch_step.offset = batch_step.offset + batch_step.limit;
-        }catch(err){
-          /*
-              We can't throw an error to Express server at this stage because the response Content-Type
-              was already sent. So we can try to attach it to the end of file.
-           */
-          console.log(err);
-          await writableStream.write(`{error : ${err.message}}\n`);
-          return;
+        //pagination
+        let batch_step = {
+          first: 2
         }
 
-      }
+        let hasNextPage = total_records > 0;
+
+        // http send stream header
+        let timestamp = new Date().getTime();
+        writableStream.writeHead(200, {'Content-Type': 'application/force-download',
+            'Content-disposition': `attachment; filename = ${timestamp}.csv`});
+
+        //get attributes names
+        let attributes = getAttributes(model_name);
+
+        //write csv header
+        let csv_header = crateHeaderCSV(attributes);
+        await writableStream.write(csv_header);
+
+        while(hasNextPage){
+
+
+            let data = await resolvers[getter_resolver]({pagination: batch_step},context);
+            let nodes = data.edges.map( e => e.node );
+            let endCursor = data.pageInfo.endCursor;
+            hasNextPage = data.pageInfo.hasNextPage;
+
+             batch_step['after'] = endCursor;
+
+             for await( record of nodes ){
+               let row = jsonToCSV(record, attributes);
+               await  writableStream.write(row);
+             }
+
+             //await new Promise(resolve => setTimeout(resolve, 5000));
+        }
+
+    }catch(err){
+      /*
+          We can't throw an error to Express server at this stage because the response Content-Type
+          was already sent. So we can try to attach it to the end of file.
+       */
+      console.log("CATCH ERROR", err);
+      //await writableStream.write(`{error : ${err.message}}\n`);
+      throw err;
+    }
 }
