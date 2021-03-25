e50a3bd99d531632979e9da10896396f5bb30646
wunderbarr <wunderbardan@gmail.com>
2021-03-25 18:58:11 +0100

    fix: connect to cassandra client by getConnectionInstances method in setup_cassandra_db.js

--- scripts/setup_cassandra_db.js
+++ scripts/setup_cassandra_db.js
@@ -1,48 +1,55 @@
-const {  getModulesSync } = require('../utils/module-helpers');
-const {  getConnection, ConnectionError } = require('../connection');
-const { join } = require('path');
-// get the default cassandra client
-const cassandraClient = getConnection("default-cassandra");
-if (!cassandraClient) throw new ConnectionError(adapter.definition);
+const { getModulesSync } = require("../utils/module-helpers");
+const { getConnectionInstances, ConnectionError } = require("../connection");
+const { join } = require("path");
 
 // require all cassandra migrations
 let migrations_cassandra = {};
-const migrationPath = "../migrations/default-cassandra"
-getModulesSync(join(__dirname, migrationPath)).forEach(file => {
+const migrationPath = "../migrations/default-cassandra";
+getModulesSync(join(__dirname, migrationPath)).forEach((file) => {
   let migration = require(`${join(migrationPath, file)}`);
-  migrations_cassandra[file.slice(0,file.length -3)] = migration
+  migrations_cassandra[file.slice(0, file.length - 3)] = migration;
 });
 
-// This only does the migration once and creates the db_migrated table. migrations won't be rerun for newly added models and/or attributes. 
+// This only does the migration once and creates the db_migrated table. migrations won't be rerun for newly added models and/or attributes.
 async function createTableMigrated() {
-  const tableQuery = "SELECT table_name FROM system_schema.tables WHERE keyspace_name='sciencedb';"
+  // get the default cassandra client
+  const connectionInstances = await getConnectionInstances();
+  const cassandraClient = connectionInstances.get("default-cassandra");
+  if (!cassandraClient) throw new ConnectionError(adapter.definition);
+  const tableQuery =
+    "SELECT table_name FROM system_schema.tables WHERE keyspace_name='sciencedb';";
   let result = await cassandraClient.execute(tableQuery);
   console.log('Check for tables in keyspace "sciencedb" executed');
   let migrationTable = false;
   let migrateToDo = true;
   for (let i = 0; i < result.rowLength; i++) {
-    if (result.rows[i].table_name === 'db_migrated') {
+    if (result.rows[i].table_name === "db_migrated") {
       migrationTable = true;
-      console.log('Migration table found.');
+      console.log("Migration table found.");
     }
   }
   if (migrationTable) {
-    let queryMigration = "SELECT migrated_at FROM db_migrated;"
+    let queryMigration = "SELECT migrated_at FROM db_migrated;";
     result = await cassandraClient.execute(queryMigration);
     if (result.rowLength >= 1) {
       migrateToDo = false;
-      console.log('Migration table filled, no more migration to do.');
+      console.log("Migration table filled, no more migration to do.");
       return process.exit(0);
     }
   }
   if (migrateToDo) {
-    await Promise.allSettled(Object.values(migrations_cassandra).map(async cassandraHandler => await cassandraHandler.up()));
-    const createTable = "CREATE TABLE IF NOT EXISTS db_migrated ( migrated_at timeuuid PRIMARY KEY )";
+    await Promise.allSettled(
+      Object.values(migrations_cassandra).map(
+        async (cassandraHandler) => await cassandraHandler.up()
+      )
+    );
+    const createTable =
+      "CREATE TABLE IF NOT EXISTS db_migrated ( migrated_at timeuuid PRIMARY KEY )";
     await cassandraClient.execute(createTable);
-    console.log('Migration table created');
+    console.log("Migration table created");
     const rowInsert = "INSERT INTO db_migrated (migrated_at) VALUES (now())";
     await cassandraClient.execute(rowInsert);
-    console.log('Migration table filled.');
+    console.log("Migration table filled.");
     return process.exit(0);
   }
 }
