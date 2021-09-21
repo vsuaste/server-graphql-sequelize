fe04568f07483ca6e581943c6cf9321877b0835f
wunderbarr <wunderbardan@gmail.com>
2021-09-21 12:06:11 +0200

    fix: parseInt for non-string environment variables

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 /**
  * Mandatory variables
@@ -7,10 +7,10 @@ const ALLOW_ORIGIN = process.env.ALLOW_ORIGIN;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!ALLOW_ORIGIN || !JWT_SECRET) {
-  throw new Error('Some mandatory environment variables have not been set\n', {
+  throw new Error("Some mandatory environment variables have not been set\n", {
     ALLOW_ORIGIN,
     JWT_SECRET,
-  })
+  });
 }
 
 /**
@@ -23,12 +23,15 @@ const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
 const MAIL_SERVICE = process.env.MAIL_SERVICE;
 
 if (!MAIL_ACCOUNT || !MAIL_HOST || !MAIL_PASSWORD || !MAIL_SERVICE) {
-  console.warn('WARNING: BulkAdd email service has not been properly configured', {
-    MAIL_ACCOUNT,
-    MAIL_HOST,
-    MAIL_PASSWORD,
-    MAIL_SERVICE,
-  })
+  console.warn(
+    "WARNING: BulkAdd email service has not been properly configured",
+    {
+      MAIL_ACCOUNT,
+      MAIL_HOST,
+      MAIL_PASSWORD,
+      MAIL_SERVICE,
+    }
+  );
 }
 
 /**
@@ -36,22 +39,23 @@ if (!MAIL_ACCOUNT || !MAIL_HOST || !MAIL_PASSWORD || !MAIL_SERVICE) {
  */
 
 // Listening port
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || 3000);
 
 // Logging
-const ERROR_LOG = process.env.ERROR_LOG || 'compact';
+const ERROR_LOG = process.env.ERROR_LOG || "compact";
 
 // Request Limits
-const LIMIT_RECORDS = process.env.LIMIT_RECORDS || 10000;
-const POST_REQUEST_MAX_BODY_SIZE = process.env.POST_REQUEST_MAX_BODY_SIZE || '1mb';
+const LIMIT_RECORDS = parseInt(process.env.LIMIT_RECORDS || 10000);
+const POST_REQUEST_MAX_BODY_SIZE =
+  process.env.POST_REQUEST_MAX_BODY_SIZE || "1mb";
 
 // Security
 const REQUIRE_SIGN_IN = process.env.REQUIRE_SIGN_IN === "false" ? false : true;
-const SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS || 10);
 
 // Timeouts
-const MAX_TIME_OUT = process.env.MAX_TIME_OUT || 2000;
-const EXPORT_TIME_OUT = process.env.EXPORT_TIME_OUT || 3600
+const MAX_TIME_OUT = parseInt(process.env.MAX_TIME_OUT || 2000);
+const EXPORT_TIME_OUT = parseInt(process.env.EXPORT_TIME_OUT || 3600);
 
 const config = {
   LIMIT_RECORDS,
@@ -68,6 +72,6 @@ const config = {
   MAIL_ACCOUNT,
   MAIL_PASSWORD,
   EXPORT_TIME_OUT,
-}
+};
 
-module.exports = config
+module.exports = config;
